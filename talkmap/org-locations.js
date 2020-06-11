f92c17cb311d5a6eeb06d0d5f8e16fed9dc7d917
Priscylla Silva <mss.priscylla@gmail.com>
2020-06-11 03:06:54 -0300

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Maceió, Brazil",
+    -9.633813, 
+    -35.708664
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Campinas, SP, Brazil",
+    -22.816646,
+    -47.064356
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Uberlândia, Brazil",
+    -18.953544,
+    -48.277129
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Kingston, Jamaica",
+    18.005351,
+    -76.750166
   ]
 ];
